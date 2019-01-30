738d41ad5dea2d410886e5800c487eea6b63beac
571730 <571730@stud.hvl.no>
2019-01-30 18:52:09 +0100

    Updates

---
+++ precache-manifest.1cef41fecb478606a63a7017d7dad713.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/workshop/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a66ac1f97ff004b272d2",
+    "url": "/workshop/static/js/runtime~main.a66ac1f9.js"
+  },
+  {
+    "revision": "ce0f8f908234693aac71",
+    "url": "/workshop/static/js/main.ce0f8f90.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/workshop/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "ce0f8f908234693aac71",
+    "url": "/workshop/static/css/main.3a4d62a7.chunk.css"
+  },
+  {
+    "revision": "d09a8d26e96462d6a69923781a21cc4d",
+    "url": "/workshop/index.html"
+  }
+];
